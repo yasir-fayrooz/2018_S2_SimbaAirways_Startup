e409f624dfeb07727886aa3bb3a80e9c0baa0d0b
Yasir Fayrooz <30676134+Crazeyheadshot@users.noreply.github.com>
2018-09-04 17:34:41 +1000

    Finished adding the baggage class
    - 4 instance variables.
    - Baggage constructor
    - collect method of boolean to verify collection dates
    - getDetails() method to get baggage details
    -toString() method to get baggage information in computational form.

--- src/airline/Baggage.java
+++ src/airline/Baggage.java
@@ -19,6 +19,80 @@ public class Baggage
 	{
 		checkInDate = new DateTime();
 		this.id = id + "_" + passengerId + "_" + checkInDate.getEightDigitDate();
+		this.weight = weight;
+	}
+	
+	
+	public boolean collect(DateTime collectionDate)
+	{
+		if(collectedDate != null ||
+		   collectedDate != null && collectionDate.equals(checkInDate))
+		{
+			return false;
+		}
+		else
+		{
+			collectedDate = collectionDate;
+			return true;
+		}
+	}
+	
+	public String getDetails()
+	{
+		String hireId = String.format("%-17s %s\n", "Hire ID:", id);
+		String baggageId = String.format("%-17s %s\n","Baggage Id:", id);
+		String weight = String.format("%-17s %s\n","Weight:", this.weight + "kg");
+		
+		String checkedIn = String.format("%-17s %s\n", "Checked in:", 
+										checkInDate.getFormattedDate());
+
+		
+		if(collectedDate != null)
+		{
+			String collected = String.format("%-17s %s\n","Collected:", 
+					collectedDate.getFormattedDate());
+			
+			String checkedCollected = hireId + 
+					  weight + 
+					  checkedIn + 
+					  collected;
+			
+			return checkedCollected;
+		}
+		else
+		{
+			
+			String checkedNotCollected = baggageId + 
+					 weight + 
+					 checkedIn;
+			
+			return checkedNotCollected;
+		}
+		
+	}
+	
+	public String toString()
+	{
+		if(collectedDate != null)
+		{
+			String checkedCollected = 
+				   id + 
+				   ":" + weight + 
+				   ":" + checkInDate.getEightDigitDate() + 
+				   ":" + collectedDate.getEightDigitDate();
+			
+			return checkedCollected;
+		}
+		else
+		{
+			String checkedNotCollected = 
+				   id + 
+				   ":" + weight + 
+				   ":" + checkInDate.getEightDigitDate() +
+				   ":" + "NO";
+			
+			return checkedNotCollected;	
+		}
 	}
 
 }
--- src/main/Driver.java
+++ src/main/Driver.java
@@ -1,9 +1,15 @@
 package main;
 
+import airline.Baggage;
+import utilities.DateTime;
+
 public class Driver 
  {
 	public static void main(String[] args)  
 	{
-		
+		DateTime Date = new DateTime();
+		Baggage baggage = new Baggage("EQF1", "RICCAV", 12.53434, Date);
+		System.out.println(baggage.getDetails());
+		System.out.println(baggage.toString());
 	}
 }
