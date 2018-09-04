e482e80d59ec11a45c54d5dd91b307856c5397e0
Yasir Fayrooz <30676134+Crazeyheadshot@users.noreply.github.com>
2018-09-04 19:33:12 +1000

    Started work on the booking class

--- src/airline/Booking.java
+++ src/airline/Booking.java
@@ -7,8 +7,76 @@ package airline;
  */
 public class Booking
 {
+	//Baggage information
+	private String baggageId;
+	private Baggage[] checkedBaggage;
+	private Baggage[] previousBaggagesChecked;
 	
 	
+	//Seat information
+	private String rowNumber;
+	private String seatNumber;
+	private boolean isExitRowSeat = false;
+	
+	//Booking cost
+	private double bookingFare;
+	
+	//Name of passenger
+	private String firstName;
+	private String lastName;	
+	
+	
+	public Booking(String id, String rowNumber, String seatNumber, double fee) 
+	{
+		if(checkRowNumberSeatNumber(rowNumber, seatNumber) == false)
+		{
+			System.out.print("Error: Booking row # or seat # out of range.");
+			return;
+		}
+		else
+		{
+			this.rowNumber = rowNumber;
+			this.seatNumber = seatNumber;
+			switch(seatNumber)
+			{
+			case "3":
+				isExitRowSeat = true;
+				break;
+			case "4":
+				isExitRowSeat = true;
+				break;
+			case "6":
+				isExitRowSeat = true;
+				break;
+			case "7":
+				isExitRowSeat = true;
+				break;
+			}
+		}
+	}
+	
+	private boolean checkRowNumberSeatNumber(String rowNumber, String seatNumber)
+	{
+		String[] seatsIndex = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
+		String[] rowIndex = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+		
+		for(int i = 0; i < seatsIndex.length; i++)
+		{
+			if(!seatNumber.equals(seatsIndex[i]))
+			{
+				return false;
+			}
+		}
+		for(int i = 0; i < rowIndex.length; i++)
+		{
+			if(!rowNumber.equals(rowIndex[i]))
+			{
+				return false;
+			}
+		}
+		
+		return true;
+	}
 	
 }
 
