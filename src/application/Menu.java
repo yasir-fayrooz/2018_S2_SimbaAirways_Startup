490aaac41870af8a0ccd0ecb516b9f025a5e7164
Yasir Fayrooz <30676134+Crazeyheadshot@users.noreply.github.com>
2018-09-13 12:44:31 +1000

    Started work on the menu class

--- src/application/Menu.java
+++ src/application/Menu.java
@@ -1,12 +1,26 @@
 package application;
 
+import airline.Facade;
+
 /*
  * Class:			Menu
  * Description:		The class represents a menu that the user will use to
  * 					interact with the application class via a facade. 
- * Author:			[student name] - [student number]
+ * Author:			[Yasir Fayrooz Ali] - [s3742162]
  */
 public class Menu
 {
+	
+	Facade facade = new Facade();
+	
+	public void run()
+	{
+		printMenu();
+	}
+	
+	private void printMenu()
+	{
+		String addBooking = String.format("%-17s %s\n", "Add Booking:", "AB");
+	}
 	
 }
